a5251b2b855f3c857c8f3f38ee57c63779492428
Kennedy Ochieng <otiken001@gmail.com>
2023-03-20 02:11:20 -0700

    0-object_like_macro.h

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: Write a program that prints  all possible
+ * diffrent combinations of two digits
+ * Numbers must be separated by ,, followed by a space
+ * The two digits must be duffrent
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * Print only the smallest combination of two digits
+ * Numbers should be printed in ascending order, with two digits
+ * You can ony use the putchar function
+ * (every other function (printf, puts, etc...) is forbidden)
+ * You can only use the putchar five times maximum in yiur code
+ * You are not allowed to use any variable of type char
+ * All your code should be inthe main function
+ * Return: 0
+ */
+
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,18 +1,24 @@
 #include "main.h"
 
 /**
- * _strcpy - Copy a string
- * @dest: Destination value
- * @src: Source value
- * Return: the pointer to dest
+ * char _strcpy - a function that copies the  string pointed to by src.
+ * @dest: Copy to.
+ * @src: Copy from.
+ * Return: string
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i;
-	for (i = 0; src[i] != '\0'; i++)
+	int 1 = 0;
+	int x = 0;
+	
+	while (*(src + 1) != '\0')
 	{
-		dest[i] = src[i];
+		1++;
 	}
-	dest[i++] = '\0';
+        for (; x < 1 ; x++)
+	{
+		desy[x] = src[x];
+	}
+	dest[1] = '\0';
 	return (dest);
 }
