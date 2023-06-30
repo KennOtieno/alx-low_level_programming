563133e56c5f148d3689d905600df055a317711e
KennOtieno <otiken001@gmail.com>
2023-06-30 06:09:24 -0700

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,4 +1,4 @@
-#define LISTS_H
+#ifndef LISTS_H
 #define LISTS_H
 
 /**
@@ -22,4 +22,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
+#endif /* LISTS_H */
